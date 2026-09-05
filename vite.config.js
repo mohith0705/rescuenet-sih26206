eeb13e394511abe84e2587a58baa3960cd9b06a9
mohith07050 <b.sai2024@nst.rishihood.edu.in>
2026-09-06 00:33:43 +0530

    Allow all hosts in vite config for global tunnel

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    open: true
+    port: 3001,
+    host: '0.0.0.0',
+    allowedHosts: 'all'
   }
 })
